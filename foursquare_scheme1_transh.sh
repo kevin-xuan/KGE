075c4b27d9c7183c412c818aad50bbf4e13f1e6f
raoxuan <raoxuanzzz@gmail.com>
2022-01-02 09:44:30 +0800

    commit

--- foursquare_scheme1_transh.sh
+++ foursquare_scheme1_transh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
 #SBATCH --partition=batch
-#SBATCH -J gowalla_transh
-#SBATCH -o gowalla_transh.%J.out
-#SBATCH -e gowalla_transh.%J.err
+#SBATCH -J foursquare_scheme1_transh
+#SBATCH -o foursquare_scheme1_transh.%J.out
+#SBATCH -e foursquare_scheme1_transh.%J.err
 #SBATCH --mail-user=peng.han@kaust.edu.sa
 #SBATCH --mail-type=ALL
 #SBATCH --time=48:30:00
@@ -16,4 +16,4 @@ module load cudnn/7.5.0-cuda10.1.105
 module load nccl/2.4.8-cuda10.1
 cd /home/hanp/hanp/research/UESTC/xuan_rao/KGE
 pip install -r requirements.txt --target=/home/hanp/anaconda3/envs/rx_torch/lib/python3.7/site-packages
-/home/hanp/anaconda3/envs/rx_torch/bin/python run_knowledge_representation.py -model_type transh -dataset gowalla -batch_size 128 -optimizer_type Adam -version gowalla_scheme2 -load_ckpt_file gowalla-transe-1639638775.ckpt
+/home/hanp/anaconda3/envs/rx_torch/bin/python run_knowledge_representation.py -model_type transh -dataset foursquare -batch_size 128 -optimizer_type Adam -version foursquare_scheme1 -load_ckpt_file foursquare-transe-1641019718.ckpt
